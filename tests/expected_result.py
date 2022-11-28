45651ce4465dbe9f4c61ac057b313e9b169c6952
Adam Ližičiar <adamliziciar27@gmail.com>
2022-11-28 20:55:23 +0100

    Successfully tested code

--- tests/expected_result.py
+++ tests/expected_result.py
@@ -1,12 +1,12 @@
 login="xlizic00"
-if(login[0] != "x"):print("ERROR!!! Login must start with x")
+#if(login[0] != "x"):print("ERROR!!! Login must start with x")
 s=""
 for i, x in enumerate(login):
     if x in "0123456789":break
     if x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":print("ERROR!!! Login should be lowercase")
     if i&1 == 0:
-        offset = ord(login[1])-ord("a")+1
+        offset = 108-ord("a")+1
     else:
-        offset = -(ord(login[2])-ord("a")+1)
+        offset = -(105-ord("a")+1)
     s+=chr((ord(x)-ord("a")+offset)%26+ord("a"))
 print(s)
